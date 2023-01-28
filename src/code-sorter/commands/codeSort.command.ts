30c85a0ea57ab8f7ed58902f5f3e217828974c35
safdar-azeem <safdarazeem00@gmail.com>
2023-01-28 21:55:00 +0000

    Add codeSort.command.ts

---
+++ src/code-sorter/commands/codeSort.command.ts
@@ -0,0 +1,33 @@
+import { codeSorter } from '..';
+import * as vscode from 'vscode';
+import { formatVue } from '../format/vue';
+
+export const codeSortCommand = vscode.commands.registerCommand('codeSorter.codeSort', async () => {
+   const editor = vscode.window.activeTextEditor;
+   if (!editor) {
+      vscode.window.showInformationMessage('No editor is active');
+      return;
+   }
+   const selection = editor.selection;
+
+   if (editor.selection.isEmpty && editor.document.languageId === 'vue') {
+      await formatVue(editor.document);
+   } else if (editor.selection.isEmpty) {
+      vscode.window.showInformationMessage('No text selected');
+   } else {
+      let text: string = editor.document.getText(editor.selection);
+
+      const workspaceEdit = new vscode.WorkspaceEdit();
+      const documentUri = editor.document.uri;
+
+      const lines = text.split('\n');
+
+      const sortedElements = codeSorter(lines);
+
+      workspaceEdit.replace(documentUri, selection, sortedElements);
+
+      vscode.workspace.applyEdit(workspaceEdit).then(() => {
+         vscode.commands.executeCommand('editor.action.formatDocument');
+      });
+   }
+});
