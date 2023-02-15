4a3672c236cd6fab2dfd8cecc84a70d48edbe65f
safdar-azeem <safdarazeem00@gmail.com>
2023-02-15 06:55:00 +0000

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,5 @@ import { codeSortCommand } from './code-sorter/commands/codeSort.command';
 export function deactivate() {}
 export function activate(context: vscode.ExtensionContext) {
    const commands = [codeSortCommand];
-
    context.subscriptions.push(...commands);
 }
