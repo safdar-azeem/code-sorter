fb4bbdbfd2b1bf8059542ba834c3e8f9dc3e2b63
safdar-azeem <safdarazeem00@gmail.com>
2023-06-26 16:36:34 +0500

    Add index.ts

---
+++ src/code-sorter/index.ts
@@ -0,0 +1,32 @@
+import { distributeFunctions, distributeVariables, extractElements, sortElements } from "./helpers";
+import { removeEmptyLines } from "./utils/array.utils";
+
+export const codeSorter = (lines: string[]): string[] => {
+    const { imports, requires, variables, functions, interfaces, types, enums, others } = extractElements(removeEmptyLines(lines));
+
+    const sortedVariables = [...Object.values(distributeVariables(variables)).map(sortElements)]
+        .filter((item) => item.length > 0)
+        .map((item) => [...item, ''])
+        .flat();
+
+    const sortedFunctions = [...Object.values(distributeFunctions(functions)).map(sortElements)]
+        .filter((item) => item.length > 0)
+        .map((item) => [...item, ''])
+        .flat();
+
+    const sortedElements = [
+        sortElements(imports),
+        sortElements(requires),
+        sortElements(types),
+        sortElements(enums),
+        sortElements(interfaces),
+        sortedVariables,
+        sortedFunctions,
+        sortElements(others),
+    ].filter((item) => item.length > 0)
+        .map((item) => [...item, ''])
+        .flat();
+
+
+    return sortedElements;
+}
