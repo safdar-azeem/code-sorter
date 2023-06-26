2f065cf77bac0ada77e8f1a4579d69e21243e029
safdar-azeem <safdarazeem00@gmail.com>
2023-06-26 22:48:08 +0500

    Update string.utils.ts

--- src/code-sorter/utils/string.utils.ts
+++ src/code-sorter/utils/string.utils.ts
@@ -1,1 +1,4 @@
 export const removeLastEmptyLines = (str: string): string => str.replace(/\n+$/, '');
+
+export const isDocuemntSelectorRegex = /^(?:let|const)\s*\w+\s*=\s*(?:document\.querySelector|document\.getElementsBy\w+|document\.getElementById|document\.querySelectorAll)\(['"][\.\w-]+['"]\);$/;
+
