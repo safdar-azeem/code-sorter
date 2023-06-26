9f948c3cd4c1b7578670529cedfa3a1c3bcfa1da
safdar-azeem <safdarazeem00@gmail.com>
2023-06-26 16:36:35 +0500

    Add index.ts

---
+++ src/code-sorter/types/index.ts
@@ -0,0 +1,27 @@
+export interface VariableCategories {
+    undefinedVariables: string[];
+    numbers: string[];
+    strings: string[];
+    arrays: string[];
+    objects: string[];
+    functionCalls: string[];
+    hooks: string[];
+}
+
+export interface FunctionCategories {
+    shorthandArrowFunctions: string[];
+    arrowFunctions: string[];
+    functionExpressions: string[];
+    functionDeclarations: string[];
+}
+
+export interface ExtractedElements {
+    imports: string[];
+    requires: string[];
+    variables: string[];
+    functions: string[];
+    interfaces: string[];
+    types: string[];
+    enums: string[];
+    others: string[];
+}
