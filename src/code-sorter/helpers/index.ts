aa1727b3da510e405eaad2305bc0e752ebde5fe8
safdar-azeem <safdarazeem00@gmail.com>
2023-06-27 22:17:08 +0500

    Update index.ts

--- src/code-sorter/helpers/index.ts
+++ src/code-sorter/helpers/index.ts
@@ -2,4 +2,3 @@ export * from "./distributeFunctions";
 export * from "./distributeVariables";
 export * from "./extractElements";
 export * from "./sortElements";
-export * from "./distributeImports";
